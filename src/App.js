79e574d6dcfc8ff0798f3a2321b9f3aff48b0ed2
bondarkoff <yehor.bondarkoff@gmail.com>
2022-12-17 18:13:22 +0200

    react_task-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
+import { Component} from 'react';
+import styled from 'styled-components';
+
 import './App.css';
+
+const EmpItem = styled.div `
+  padding: 20px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0,0,0, 0.25);
+  a {
+    display: block;
+    margin: 10px 0 10px 0;
+    color: ${props => props.active ? 'orange' : 'black'};
+  }
+  input {
+    display: block;
+    margin-top: 10px;
+  }
+`
+
+const Header = styled.h2 `
+  font-size: 22px;
+`
+
+const Button = styled.button `
+  display: block;
+  padding: 5px 15px;
+  background-color: gold;
+  border: 1px solid rgba(0,0,0, 0.2);
+  box-shadow: 5px 5px 10px rgba(0,0,0, 0.2);
+`
+
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 27,
+      plus: '+++',
+      minus: '---',
+      position: ''
+    }
+  }
+
+  nextYear = () => {
+    this.setState(state => ({
+        years: state.years + 1,
+        plus: state.plus + '+'
+    }))
+  }
+
+  commitInputChanges = (e, color) => {
+    console.log(color);
+    this.setState({
+      position: e.target.value
+    })
+  }
+
+  render () {
+    const {name, surname, link} = this.props;
+    const {position, years} = this.state;
+    return (
+      <EmpItem active>
+        <Button onClick={this.nextYear}>{this.state.plus}</Button>
+        <Header>My Name Is {name}, Surname - {surname}, Age - {years}, position - {position}</Header>
+        <a href={link}>My profile</a>
+        <form>
+          <span>Введите должность</span>
+          <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')}/>
+        </form>
+      </EmpItem>
+    )
+  }
+}
+
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto 0 auto;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <WhoAmI name="Yehor" surname="Bondarkov" link="bondarkoff.github.io."/>
+      <WhoAmI name="Alex" surname="Moman" link="twitter.com "/>
+    </Wrapper>
   );
 }
 
---
+++ src/BootstrapTest.js
@@ -0,0 +1,81 @@
+import {Container, Row, Col, Carousel, Accordion} from 'react-bootstrap'
+
+const BootstrapTest = () => {
+    return (
+        <Container className='mt-5 mb-5'>
+        <Row>
+          <Col>
+            <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>Accordion Item #1</Accordion.Header>
+                    <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>Accordion Item #2</Accordion.Header>
+                    <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                    minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in
+                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
+          </Col>
+          <Col>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="https://upload.wikimedia.org/wikipedia/commons/9/91/JavaScript_screenshot.png"
+                    alt="First slide"
+                    />
+                    <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="https://resources.jetbrains.com/help/img/idea/2022.3/ws_react_jsx_tree_highlighting.png"
+                    alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src="https://cdn-media-2.freecodecamp.org/w1280/5f9c9f1e740569d1a4ca40e6.jpg"
+                    alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+          </Col>
+        </Row>
+      </Container>
+    )
+}
+
+export default BootstrapTest;
